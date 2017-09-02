aa331c442e969998ec63af8b1bf22690ee4fa76b
DZ Chan <cdzos97@gmail.com>
2017-09-02 16:48:33 +0800

    read and write FAT

--- index.js
+++ index.js
@@ -4,28 +4,55 @@
  ************************/ 
 
 const fs = require("fs");
-const FAT_LENGTH = 256;
-const FILEDISK_SIZE = 2 * 1024;
+const BLOCK_SIZE = 4 * 1024;
+const BLOCK_NUM = 256
+const FILEDISK_SIZE = BLOCK_NUM * BLOCK_SIZE;
 
 let FATBuffer = [];
 let FileDiskHandle;
 
-for (let i = 0; i < FAT_LENGTH; i++) {
+for (let i = 0; i < BLOCK_NUM; i++) {
     FATBuffer.push(0);
+}
+
+FATBuffer[0] = 1;
+FATBuffer[1] = -1;
+
+function ReadFAT(fd, fatBuffer) {
+    let bytesNumber = 256 * 2;
+    let buffer = Buffer.alloc(bytesNumber);
+    let offset = 0;
+    while (offset < bytesNumber) {
+        offset += fs.readSync(FileDiskHandle, buffer, offset, bytesNumber - offset, offset);
+    }
+}
+
+function WriteFAT(fd, fatBuffer) {
+    let bytesNumber = 256 * 2;
+    let buffer = Buffer.alloc(bytesNumber);
+    for (let i = 0; i < 256; i++) {
+        buffer.writeInt16BE(FATBuffer[i], i * 2, true);
+    }
+    let offset = 0;
+    while (offset < bytesNumber) {
+        offset += fs.writeSync(FileDiskHandle, buffer, offset, bytesNumber - offset, offset);
+    }
 }
 
 exports.connect = (filedisk) => {
     if (fs.existsSync(filedisk)) {
-        FileDiskHandle = fs.openSync(filedisk, 'w+');
+        FileDiskHandle = fs.openSync(filedisk, 'r+');
+        ReadFAT(FileDiskHandle, FATBuffer);
     } else {
         // file not exist
         // make a 2M null file
         FileDiskHandle = fs.openSync(filedisk, 'w+');
         let buffer = Buffer.alloc(256);
-        for (let i = 0; i < 8; i++) {
+        for (let i = 0; i < FILEDISK_SIZE / 256; i++) {
             fs.writeFileSync(FileDiskHandle, buffer);
             fs.writeSync(FileDiskHandle, buffer, 0, 256, i * 256);
         }
+        WriteFAT(FileDiskHandle, FATBuffer);
     }
 }
 
@@ -39,4 +66,8 @@ exports.open = (filname) => {
 
 exports.read = () => {
     return 0;
+}
+
+exports.getFATValue = (index) => {
+    return FATBuffer[index];
 }
