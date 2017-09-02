a22d4d983887ec636b6f7f354ce763d16ed2630e
DZ Chan <cdzos97@gmail.com>
2017-09-02 17:09:41 +0800

    dirstruct layout

---
+++ dirstruct.js
@@ -0,0 +1,43 @@
+/************************ 
+ * Author: DZ Chan 
+ * Date:   2017-09-02 
+ ************************/ 
+
+const fs = require('fs');
+
+function readDirStructFromBuffer(buffer) {
+
+}
+
+function writeDirStructToBuffer(dirStruct) {
+
+}
+
+class DirItem {
+
+    constructor() {
+
+    }
+
+    toBytes() {
+
+    }
+
+}
+
+class DirStruct {
+
+    constructor() {
+        this._data = []
+    }
+
+    toBytes() {
+
+    }
+
+};
+
+exports.readDirStructFromBuffer = readDirStructFromBuffer;
+exports.writeDirStructToBuffer = writeDirStructToBuffer;
+exports.DirItem = DirItem;
+exports.DirStruct = DirStruct;
